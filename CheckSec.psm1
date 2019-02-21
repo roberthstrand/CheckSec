7bbae63741ed18a4b96624f522703344825a15fe
Roberth Strand <roberth@strandkonsult.no>
2019-02-21 22:09:52 +0100

    Merge two cmdlets

--- CheckSec.psm1
+++ CheckSec.psm1
@@ -5,7 +5,7 @@ function checkDomain {
         $domain
     )
     try {
-        Write-Verbose "Checking if the domain exists" #-Verbose
+        Write-Verbose "Checking if the domain exists"
         Resolve-DnsName $Domain -NoHostsFile -DnsOnly -ErrorAction stop | out-null
     }
     catch {
@@ -37,15 +37,38 @@ function Get-EmailDetail {
     )
     # Check whether domain exists
     checkDomain $Domain
-    $domainMX = Resolve-DnsName $Domain -Type MX | Select-Object NameExchange -first 1
-    $domainSPF = Resolve-DnsName $Domain -Type TXT | Where-Object -Property Strings -Like "v=spf1*" #TODO: få bort strings
+    $domainMX    = Resolve-DnsName $Domain -Type MX | Select-Object NameExchange -first 1
+    $domainSPF   = Resolve-DnsName $Domain -Type TXT | Where-Object -Property Strings -Like "v=spf1*" #TODO: få bort strings
     $domainDmarc = Resolve-DnsName "_dmarc.$Domain" -type TXT -ErrorAction SilentlyContinue
+    # SPF
     if ($domainSPF) {
         Write-Verbose "SPF, present!"
         $spfPresent = $true
+        $SpfRecord  = $domainSPF.strings.replace("{}","")
     } else {
         $spfPresent = $false
+        $SpfRecord  = "N/A"
     }
+    if ($domainSPF){
+        spfExtractor -SpfTxt $SpfRecord
+    }
+    if ($global:SpfPtr) {
+        Write-Warning "Using the PTR mechanism is not recommended!"
+        Write-Warning "Reference: RFC7208 Section 5.5."
+    }
+    if ($global:spfCounter -gt 10) {
+        Write-Warning "Too many DNS mechanics"
+    }
+    # DKIM
+    if ($emailDetail.MX -like "*outlook.com") {
+        $dkimCheck = Resolve-DnsName selector1._domainkey.$domain -DnsOnly -ErrorAction SilentlyContinue
+        if ($dkimCheck){
+            $dkimPresent = $true
+        } else {
+            $dkimPresent = $false
+        }
+    }
+    # DMARC
     if ($domainDmarc) {
         $domainDmarcPresent = $true
     } else {
@@ -63,46 +86,12 @@ function Get-EmailDetail {
     [PSCustomObject]@{
         'Domain'          = $Domain
         'MX'              = $domainMX.NameExchange
-        'SpfPresent'     = $spfPresent
-        'SpfRecord'      = $domainSPF.strings
-        'DmarcPresent' = $domainDmarcPresent
-        'DmarcPolicy'  = $dmarcPolicy
-    }
-}
-function Test-EmailSecurity {
-    param (
-        [Parameter(Position = 0, Mandatory)]
-        [string]
-        $Domain
-    )
-    $emailDetail = Get-EmailDetail $Domain
-    # SPF
-    if ($emailDetail.SpfRecord[0]){
-        spfExtractor -SpfTxt $emailDetail.SpfRecord[0]
-    }
-    Write-Output "Checking SPF"
-    if ($global:SpfPtr) {
-        Write-Warning "Using the PTR mechanism is not recommended!"
-        Write-Warning "Reference: RFC7208 Section 5.5."
-    }
-    if ($global:spfCounter -gt 10) {
-        Write-Warning "Too many DNS mechanics"
-    }
-    # DKIM
-    Write-Output "Checking DKIM"
-    if ($emailDetail.MX -like "*outlook.com") {
-        try {
-            Resolve-DnsName selector1._domainkey.$domain -DnsOnly -ErrorAction SilentlyContinue | Out-Null
-            $dkimPresent = $true
-        }
-        catch {
-            $dkimPresent = $false
-        }
-    }
-    [PSCustomObject]@{
-        "SPF"       = $emailDetail.SpfPresent
-        "Mechanics" = $global:spfCounter
-        "PTR"       = $global:SpfPtr
-        "DKIM"      = $dkimPresent
+        'SpfPresent'      = $spfPresent
+        'SpfRecord'       = $SpfRecord
+        'SpfDnsMechanics' = $global:spfCounter
+        'SpfPtrInUse'     = $global:SpfPtr
+        'DkimPresent'     = $dkimPresent
+        'DmarcPresent'    = $domainDmarcPresent
+        'DmarcPolicy'     = $dmarcPolicy
     }
 }
