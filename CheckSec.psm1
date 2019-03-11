b04323e73e15c9cb8a623c4de18b4913c32505be
Roberth Strand <roberth@strandkonsult.no>
2019-03-12 00:40:24 +0100

    Hazzah!

--- CheckSec.psm1
+++ CheckSec.psm1
@@ -1,4 +1,19 @@
-# Server security
+<#
+CheckSec - https://github.com/roberthstrand/CheckSec
+A PowerShell module for security checks related to Active Directory, DNS, E-mail and more.
+
+This is a work in progress and will most likely continue to be a work in progress. 
+I'm always open for suggestions, so feel free to add me on any social media which you frequent.
+
+Github:  @roberthstrand
+Twitter: @RoberthTweets
+
+Website: https://destruktive.one
+Blog:    https://blog.destruktive.one
+#>
+
+# Fetching actual code:
+## Server security
 . .\src\SrvCheck.ps1
-# Email security
+## Email security
 . .\src\EmailCheck.ps1
